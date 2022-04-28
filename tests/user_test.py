b4b500fb71a3dad0894e9142ac6c70a80812ac34
tlr24 <tlr24@njit.edu>
2022-04-28 00:05:37 -0400

    Updated app testing environment, made sure it passed pytest

--- tests/user_test.py
+++ tests/user_test.py
@@ -10,7 +10,7 @@ def test_adding_user(application):
         assert db.session.query(Song).count() == 0
         #showing how to add a record
         #create a record
-        user = User('keith@webizly.com', 'testtest')
+        user = User('tttt@gmail.com', '12345678')
         #add it to get ready to be committed
         db.session.add(user)
         #call the commit
@@ -18,7 +18,7 @@ def test_adding_user(application):
         #assert that we now have a new user
         assert db.session.query(User).count() == 1
         #finding one user record by email
-        user = User.query.filter_by(email='keith@webizly.com').first()
+        user = User.query.filter_by(email='tttt@gmail.com').first()
         log.info(user)
         #asserting that the user retrieved is correct
-        assert user.email == 'keith@webizly.com'
+        assert user.email == 'tttt@gmail.com'
