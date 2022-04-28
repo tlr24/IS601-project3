2b78a4c5036a16f542ffdf08db06c74a188c7e4b
tlr24 <tlr24@njit.edu>
2022-04-27 23:54:01 -0400

    Got song csv files to upload and go into the db

--- app/songs/__init__.py
+++ app/songs/__init__.py
@@ -1,15 +1,39 @@
+import csv
+import logging
+import os
 from flask import Blueprint, render_template, abort, url_for, current_app
 from flask_login import current_user, login_required
+from app.db import db
+from app.db.models import Song
 from app.songs.forms import csv_upload
 from jinja2 import TemplateNotFound
+from werkzeug.utils import secure_filename, redirect
 
 song = Blueprint('songs', __name__, template_folder='templates')
+
 
 @song.route('/songs/upload', methods=['POST', 'GET'])
 @login_required
 def song_upload():
     form = csv_upload()
+    if form.validate_on_submit():
+        log = logging.getLogger("myApp")
+        filename = secure_filename(form.file.data.filename)
+        filepath = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+        form.file.data.save(filepath)
+        # user = current_user
+        list_of_songs = []
+        with open(filepath) as file:
+            csv_file = csv.DictReader(file)
+            for row in csv_file:
+                list_of_songs.append(Song(row['Name'], row['Artist'], row['Year'], row['Genre']))
+
+        current_user.songs = list_of_songs
+        db.session.commit()
+
+        #return redirect(url_for('songs.songs_browse'))
     try:
         return render_template('upload_songs.html', form=form)
     except TemplateNotFound:
         abort(404)
+
---
+++ tests/user_test.py
@@ -0,0 +1,24 @@
+import logging
+
+from app import db
+from app.db.models import User, Song
+
+def test_adding_user(application):
+    log = logging.getLogger("myApp")
+    with application.app_context():
+        assert db.session.query(User).count() == 0
+        assert db.session.query(Song).count() == 0
+        #showing how to add a record
+        #create a record
+        user = User('keith@webizly.com', 'testtest')
+        #add it to get ready to be committed
+        db.session.add(user)
+        #call the commit
+        db.session.commit()
+        #assert that we now have a new user
+        assert db.session.query(User).count() == 1
+        #finding one user record by email
+        user = User.query.filter_by(email='keith@webizly.com').first()
+        log.info(user)
+        #asserting that the user retrieved is correct
+        assert user.email == 'keith@webizly.com'
