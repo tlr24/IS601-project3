98d0d316042520a6e9d84567912c0747cfa3128a
tlr24 <tlr24@njit.edu>
2022-04-28 21:10:42 -0400

    Added test for adding songs

--- tests/songs_test.py
+++ tests/songs_test.py
@@ -3,7 +3,7 @@ import logging
 import csv
 import os
 import pytest
-from app.db.models import db, Song
+from app.db.models import db, Song, User
 
 
 def test_csv_upload(client, add_user):
@@ -27,3 +27,25 @@ def test_csv_upload(client, add_user):
     with client.application.app_context():
         log.debug(file)
         # assert Song.query.filter_by(title="Down").first() is not None
+
+def test_adding_songs(application):
+    """Test adding songs"""
+    with application.app_context():
+        user = User('tttt@gmail.com', '12345678')
+        db.session.add(user)
+        db.session.commit()
+        # prepare songs to insert
+        user.songs = [Song("title1", "artist1", "2020", "Rap"), Song("title2", "artist2", "2022", "Pop")]
+        db.session.commit()
+        assert db.session.query(Song).count() == 2
+        song1 = Song.query.filter_by(title='title1').first()
+        assert song1.title == "title1"
+        # changing the title of the song
+        song1.title = "New Song"
+        db.session.commit()
+        song2 = Song.query.filter_by(title='New Song').first()
+        assert song2.title == "New Song"
+        # checking cascade delete
+        db.session.delete(user)
+        assert db.session.query(User).count() == 0
+        assert db.session.query(Song).count() == 0
