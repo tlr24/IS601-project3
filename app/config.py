42ecfc1a8bf92f4387842fe66f2b354fc1e9f48f
tlr24 <tlr24@njit.edu>
2022-04-28 23:06:03 -0400

    Updated uploads folder location in config

--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ class Config(object):
     DB_DIR = os.getenv('DB_DIR','database')
     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR,'..', DB_DIR, "db.sqlite")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER =  os.getenv('UPLOAD_FOLDER', BASE_DIR + '/uploads')
+    UPLOAD_FOLDER =  os.getenv('UPLOAD_FOLDER', os.path.join(BASE_DIR,'..' , 'uploads'))
     LOG_DIR =  os.path.join(BASE_DIR, '../logs')
 class ProductionConfig(Config):
     pass
